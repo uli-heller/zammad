c2e64489a6993fd3a7e4ff5aff5308fcd989e69b
Uli Heller <uli.heller@daemons-point.com>
2021-10-08 08:28:08 +0200

    Add sha256sum

--- uli-package.sh
+++ uli-package.sh
@@ -2,4 +2,4 @@
 VERSION="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 git clean -f
 git archive --format=tar "${VERSION}"|bzip2 -c9 >zammad-${VERSION}.tar.bz2
-
+sha256sum zammad-${VERSION}.tar.bz2 >zammad-${VERSION}.tar.bz2.sha256
